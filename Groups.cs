f9c873ab89526f7d2e87980e7a0ca044a32c30bb
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-03-02 20:16:16 +0000

    GIVE ME ONE GOOD REASON WHY I SHOULD NEVER MAKE A CHANGE

--- Groups.cs
+++ Groups.cs
@@ -2,4 +2,9 @@
 public class Groups
 {
     // Add your code here
+    public int[,] GroupedNumbers = {{1 ,3, 5}, {2, 4, 6}};
 }
+
+//should contain a class-level variable array named GroupedNumbers
+//should add an array containing 1, 3, and 5 to GroupedNumbers
+//should add an array containing 2, 4, and 6 to GroupedNumbers
